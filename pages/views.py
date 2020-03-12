32b55d10a71474e990fcc051a519c72f51ab42ba
Дмитро Алексєєв <alekseev@rivne.itstep.org>
2020-03-12 20:56:30 +0200

    second

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,18 @@ from django.template.response import TemplateResponse
 
 
 def home(request):
-    return TemplateResponse(request, "home.html")
+    data = {
+        "h1": "Craft",
+        "subtitle": "This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.",
+
+        "title1": "One",
+        "desc1": "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
+        "title2": "Two",
+        "desc2": "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
+        "title3": "Three",
+        "desc3": "Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.",
+    }
+    return TemplateResponse(request, "home.html", data)
 
 
 def about(request):
